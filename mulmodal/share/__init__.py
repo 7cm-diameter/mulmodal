e40fed5a7a0e0dde91781cb15b3380042a280f7a
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2023-11-09 10:38:09 +0900

    Add functions that will be used across several experiment programns

--- mulmodal/share/__init__.py
+++ mulmodal/share/__init__.py
@@ -1,2 +1,46 @@
+from time import perf_counter
+from amas.agent import Agent
+
+
 def show_progress(trial: int, isi: float):
     print(f"Trial {trial}: Cue will be presented {isi} secs after.")
+
+
+async def flush_message_for(agent: Agent, duration: float):
+    while duration >= 0. and agent.working():
+        s = perf_counter()
+        await agent.try_recv(duration)
+        duration -= perf_counter() - s
+
+
+async def fixed_interval_with_limit(agent: Agent, interval: float,
+                                    correct: str, postpone: float, limit: float):
+    _limit = limit
+    while interval >= 0. and agent.working():
+        s = perf_counter()
+        mail = await agent.try_recv(interval)
+        required_time = perf_counter() - s
+        interval -= required_time
+        if limit < 0 and interval < 0:
+            break
+        if mail is None:
+            interval = 1e-3
+            limit -= required_time
+            continue
+        _, response = mail
+        if response != correct and interval < postpone:
+            interval = postpone
+            limit = _limit
+
+
+async def fixed_interval_with_limit2(agent: Agent, interval: float,
+                                    correct: str, postpone: float, limit: float):
+    while interval >= 0. and agent.working():
+        s = perf_counter()
+        mail = await agent.try_recv(limit)
+        if mail is None:
+            break
+        interval -= perf_counter() - s
+        _, response = mail
+        if response != correct and interval < postpone:
+            interval = postpone
